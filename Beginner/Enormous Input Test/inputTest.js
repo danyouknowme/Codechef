1355ae41c6073b0308fd9c77eb0cff74de165774
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-04-23 22:54:17 +0700

    Update inputTest.js

--- Beginner/Enormous Input Test/inputTest.js
+++ Beginner/Enormous Input Test/inputTest.js
@@ -12,7 +12,7 @@ function myFunction(args) {
     let arr = args.split("\n");
     let [n, k] = arr[0].split(" ");
     let ans=0;
-    for (let i=0; i<n; i++) {
+    for (let i=1; i<n+1; i++) {
         if (Number(arr[i] % k) === 0) {
             ans++;
         }
