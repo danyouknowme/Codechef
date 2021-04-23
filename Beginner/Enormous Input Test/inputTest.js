4c9687fc9bb7499a4295b6f95ea845f35e99e503
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-04-23 17:52:15 +0700

    Create inputTest.js

---
+++ Beginner/Enormous Input Test/inputTest.js
@@ -0,0 +1,21 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (i) => {
+    let arr = i;
+});
+process.stdin.on('end', () => {
+    console.log(myFunction(arr));
+});
+
+function myFunction(args) {
+    let arr = args.split("\n");
+    let [n, k] = arr[0].split(" ");
+    let ans=0;
+    for (let i=0; i<n; i++) {
+        if (Number(arr[i] % k) === 0) {
+            ans++;
+        }
+    }
+    return ans;
+}
