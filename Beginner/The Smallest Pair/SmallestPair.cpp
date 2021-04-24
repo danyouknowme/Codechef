8df5978b210e550519f09ab9be9f0b8c19ee8c9f
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-04-24 21:23:27 +0700

    Create SmallestPair.cpp

---
+++ Beginner/The Smallest Pair/SmallestPair.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	for (int i=0; i<n; i++) {
+	    int arr_sz;
+	    cin >> arr_sz;
+	    int arr[arr_sz];
+	    for (int j=0; j<arr_sz; j++) {
+	        int a;
+	        cin >> a;
+	        arr[j] = a;
+	    }
+	    sort(arr, arr + arr_sz);
+	    cout << arr[0] + arr[1];
+	}
+}
