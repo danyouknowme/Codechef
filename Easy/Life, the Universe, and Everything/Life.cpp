7139b0013944f1bd75ef60393f27e2165b7e13c0
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-04-30 00:37:58 +0700

    Create Life.cpp

---
+++ Easy/Life, the Universe, and Everything/Life.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int num;
+	cin >> num;
+	while (num != 42) {
+	    cout << num << endl;
+	    cin >> num;
+	}
+}
